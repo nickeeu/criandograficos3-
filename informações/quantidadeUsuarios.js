d34109536cd8af595ec86bd344f0e44dddf45153
izabel <133922307+nickeeu@users.noreply.github.com>
2024-11-06 20:14:31 -0300

    quant usua cert

---
+++ informações/quantidadeUsuarios.js
@@ -0,0 +1,23 @@
+import { getCSS, tickConfig} from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeDeUsuarios,
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+        }
+    }
+]
+
+    const laytout = {
+        plot_bgcolor:
+}
