dfcf750be1b55eef7b484ef1e73323438fc59f41
izabel <133922307+nickeeu@users.noreply.github.com>
2024-11-18 22:00:01 -0300

    salvo dia 18/11 common

--- informações/common.js
+++ informações/common.js
@@ -9,4 +9,23 @@ const tickConfig = {
     color: getCSS('--primary-color')
 }
 
+function criarGrafico(data, layout) {
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    const config = {
+        responsive: true,
+        displayModeBar: false
+    }
+    Plotly.newPlot(grafico, data, layout, config)
+}
+
+function incluirTexto(texto) {
+    const container = document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = texto
+    container.appendChild(paragrafo)
+}
+
 export {getCSS, tickConfig}
